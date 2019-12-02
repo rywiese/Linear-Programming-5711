4220d7ba07387160b68449406ff1ca6202d2d57b
Ry Wiese <ry.wiese@gmail.com>
2019-12-02 16:00:00 -0600

    testing

--- HW8/Gradient_Descent_R2_R1_Exact.m
+++ HW8/Gradient_Descent_R2_R1_Exact.m
@@ -1,7 +1,8 @@
-function [minimizer] = Gradient_Descent_R2_R1_Exact(f, x, df_dx1, df_dx2, epsilon)
+function [minimizer, xs_new] = Gradient_Descent_R2_R1_Exact(f, x, df_dx1, df_dx2, epsilon, xs)
 
     if norm([df_dx1(x), df_dx2(x)]) < epsilon
         minimizer = x;
+        xs_new = xs;
         return;
     end
 
@@ -14,6 +15,8 @@ function [minimizer] = Gradient_Descent_R2_R1_Exact(f, x, df_dx1, df_dx2, epsilo
     end
     a = Golden_Section_Search(@Taylor, 0, 1, .001); % Using a smaller value of epsilon here gave me a memory error
 
-    minimizer = Gradient_Descent_R2_R1_Exact(f, x + a * d, df_dx1, df_dx2, epsilon);
+    x_new = x + a * d;
+    xs_new = [xs, x_new];
+    [minimizer, xs_new] = Gradient_Descent_R2_R1_Exact(f, x_new, df_dx1, df_dx2, epsilon, xs_new);
 
 end
--- HW8/HW8.m
+++ HW8/HW8.m
@@ -1,10 +1,13 @@
 clc; clear;
 
-minimizer = Gradient_Descent_R2_R1_Exact(@f, [0; 0], @df_dx1, @df_dx2, .00001);
+[minimizer, xs] = Gradient_Descent_R2_R1_Exact(@f, [0; 0], @df_dx1, @df_dx2, .00001, [[0; 0]]);
 
 "Problem 1 using exact line search"
 minimizer
 f(minimizer)
+[~, n] = size(xs);
+plot(xs(1,:), xs(2,:))
+
 
 function [y] = f(x)
     y = exp(1 - x(1) - x(2)) + exp(x(1) + x(2) - 1) + x(1)^2 + x(1) * x(2) + x(2)^2 + 2 * x(1) - 3 * x(2);
