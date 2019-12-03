72dd58c554eda358430aa2cad5da798ed59bf27d
ry <ry.wiese@gmail.com>
2019-12-02 19:33:30 -0600

    downloaded pdf

--- HW8/Gradient_Projection.m
+++ HW8/Gradient_Projection.m
@@ -13,7 +13,7 @@ function [minimizer, xs_new] = Gradient_Projection(f, A, x, df_dx1, df_dx2, df_d
 
     a = Backtracking_Line_Search(f, x, d, J, .5, .5);
 
-    x_new = x + a * d
+    x_new = x + a * d;
     xs_new = [xs, x_new];
     [minimizer, xs_new] = Gradient_Projection(f, A, x_new, df_dx1, df_dx2, df_dx3, epsilon, xs_new);
 
